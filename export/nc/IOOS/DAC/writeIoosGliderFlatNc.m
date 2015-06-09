a86cdacf828f7830186b43441f51cf36cf7832f6
Russell Senior <senior@ohsu.edu>
2015-06-09 12:12:27 -0700

    export/nc/IOOS/DAC/writeIoosGliderFlatNc.m: preserve any pre-existing history content, appending new
    Signed-off-by: Russell Senior <senior@ohsu.edu>

--- export/nc/IOOS/DAC/writeIoosGliderFlatNc.m
+++ export/nc/IOOS/DAC/writeIoosGliderFlatNc.m
@@ -259,7 +259,7 @@ if isequal(I,0)
     I = length(nci.Attributes) + 1;
     nci.Attributes(I).Name = 'history';
 end
-nci.Attributes(I).Value = [datestr(now, 'yyyy-mm-ddTHH:MM:SSZ') ' ' mfilename('fullpath') '.m'];
+nci.Attributes(I).Value = [ nci.Attributes(I).Value, datestr(now, 'yyyy-mm-ddTHH:MM:SSZ') ' ' mfilename('fullpath') '.m' ];
 % title and id identifier
 id = sprintf('%s-%s',...
     pStruct.meta.glider,...
